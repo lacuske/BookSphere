cfdffb42ee3b8ea002b36280c7a0139598abfb04
Ree Li <li.mingt@northeastern.edu>
2024-03-19 11:14:34 -0700

    adding publisher

---
+++ src/main/java/booksphere/model/Publisher.java
@@ -0,0 +1,47 @@
+package booksphere.model;
+
+public class Publisher {
+    private int publisherID;
+    private String publisherName;
+    private String biography;
+    
+    
+    public Publisher(String publisherName, String biography) {
+        this.publisherName = publisherName;
+        this.biography = biography;
+    }
+    
+    public int getPublisherID() {
+        return publisherID;
+    }
+    
+    public void setPublisherID(int publisherID) {
+        this.publisherID = publisherID;
+    }
+    
+    public String getPublisherName() {
+        return publisherName;
+    }
+    
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+    
+    public String getBiography() {
+        return biography;
+    }
+    
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+    
+    // toString() method
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "publisherID=" + publisherID +
+                ", publisherName='" + publisherName + '\'' +
+                ", biography='" + biography + '\'' +
+                '}';
+    }
+}
